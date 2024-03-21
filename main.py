97f4fe98b1675c764d5d626f64db7d552618bf3a
inc3gnito <inc3gnito@gmail.com>
2024-03-21 10:45:27 +0100

    version 8

--- main.py
+++
@@ -1,9 +0,0 @@
-from pyscript import Element
-
-def noveld():
-    output = Element('output').element
-    ertek = output.innerHTML
-    szam = int(ertek) +1
-    output.innerHTML =szam
-    print(szam)
-    
